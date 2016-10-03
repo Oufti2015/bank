c54de41f4ae68681065ec9b0b451caab31e54b14
Stéphane Stiennon <stephane.stiennon@gmail.com>
2016-10-03 10:25:54 +0200

    format counterparties

--- src/main/java/sst/bank/activities/b/parsing/OperationFiller.java
+++ src/main/java/sst/bank/activities/b/parsing/OperationFiller.java
@@ -9,6 +9,13 @@ import sst.bank.model.container.BankContainer;
 
 public class OperationFiller implements BankActivity {
 
+    private Pattern[] patternDetails = { Pattern.compile(".*((BE)\\d{14}).*"), Pattern.compile(".*((LU)\\d{18}).*"),
+	    Pattern.compile(".*((FR)\\d{18}).*") };
+    private Pattern[] patternFormatted = { Pattern.compile(".*((BE)\\d{2} \\d{4} \\d{4} \\d{4}).*"),
+	    Pattern.compile(".*((LU)\\d{2} \\d{4} \\d{4} \\d{4} \\d{4}).*"),
+	    Pattern.compile(".*((FR)\\d{2} \\d{4} \\d{4} \\d{4} \\d{4}).*") };
+    private String[] countries = { "BE", "LU", "FR}" };
+
     @Override
     public void run() {
 	BankContainer.me().operations().stream().forEach(o -> fillOperation(o));
@@ -21,29 +28,32 @@ public class OperationFiller implements BankActivity {
     }
 
     private void defaultCounterparty(Operation o) {
-	if (o.getCounterparty() == null) {
-	    Pattern pattern = Pattern.compile(".*((LU|BE)\\d{14}).*");
-	    Matcher matcher = pattern.matcher(o.getDetail());
-	    if (matcher.find()) {
-		o.setCounterparty(matcher.group(1));
-	    } else {
-		pattern = Pattern.compile(".*((LU|BE)\\d{2} \\d{4} \\d{4} \\d{4}).*");
-		matcher = pattern.matcher(o.getDetail());
-		if (matcher.find()) {
-		    o.setCounterparty(matcher.group(1));
-		}
-	    }
+	if (o.getCounterparty() != null && o.getCounterparty().startsWith("LU") && o.getCounterparty().length() == 16) {
+	    o.setCounterparty(null);
 	}
 
-	if (o.getCounterparty() != null) {
-	    o.setCounterparty(formatCounterparty(o.getCounterparty()));
+	for (int i = 0; i < patternDetails.length; i++) {
+	    if (o.getCounterparty() == null) {
+		Matcher matcher = patternDetails[i].matcher(o.getDetail());
+		if (matcher.find()) {
+		    o.setCounterparty(matcher.group(1));
+		} else {
+		    matcher = patternFormatted[i].matcher(o.getDetail());
+		    if (matcher.find()) {
+			o.setCounterparty(matcher.group(1));
+		    }
+		}
+	    }
+
+	    if (o.getCounterparty() != null) {
+		o.setCounterparty(formatCounterparty(o.getCounterparty(), countries[i], patternFormatted[i]));
+	    }
 	}
     }
 
-    private String formatCounterparty(String group) {
-	Pattern pattern = Pattern.compile(".*((LU|BE)\\d{2} \\d{4} \\d{4} \\d{4}).*");
+    private String formatCounterparty(String group, String country, Pattern pattern) {
 	Matcher matcher = pattern.matcher(group);
-	if (!matcher.find() && group.startsWith("BE")) {
+	if (!matcher.find() && group.startsWith(country)) {
 	    int i = 0;
 	    String result = "";
 	    while (i < group.length()) {
@@ -54,5 +64,4 @@ public class OperationFiller implements BankActivity {
 	}
 	return group;
     }
-
 }
