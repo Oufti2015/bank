24a4c89b06cd754641f9cee8e3e7461b154eeb97
Stéphane Stiennon <stephane.stiennon@gmail.com>
2017-06-07 08:38:54 +0200

    add test for operations loader

---
+++ src/test/java/sst/bank/config/TestOperationsLoader.java
@@ -0,0 +1,30 @@
+package sst.bank.config;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import sst.bank.activities.BankActivity;
+import sst.bank.activities.a.config.CategoriesLoader;
+import sst.bank.activities.a.config.Configurator;
+import sst.bank.activities.a.config.LabelsLoader;
+import sst.bank.activities.b.loading.OperationsLoader;
+import sst.bank.model.container.BankContainer;
+
+public class TestOperationsLoader {
+
+    @Before
+    public void init() {
+	new CategoriesLoader().run();
+	new LabelsLoader().run();
+	new Configurator().run();
+    }
+
+    @Test
+    public void testOperationsLoader() {
+	BankActivity activity = new OperationsLoader();
+	activity.run();
+
+	Assert.assertEquals(800, BankContainer.me().operationsContainer().operations().size());
+    }
+}
