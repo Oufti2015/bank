0f4068e5a98eeda142bb87993f64021998720dab
Stéphane Stiennon <stephane.stiennon@gmail.com>
2017-06-19 09:40:41 +0200

    Project
    Add toString and compare

--- src/main/java/sst/bank/model/Project.java
+++ src/main/java/sst/bank/model/Project.java
@@ -2,19 +2,32 @@ package sst.bank.model;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
 import java.util.Set;
 import java.util.TreeSet;
 
 import lombok.Data;
-import lombok.ToString;
 
 @Data
-@ToString(exclude = "operations")
-public class Project implements Serializable {
+public class Project implements Serializable, Comparable<Project> {
     private String id;
     private String name;
     private LocalDate startDate;
     private LocalDate endDate;
 
     private Set<Operation> operations = new TreeSet<>();
+
+    @Override
+    public int compareTo(Project o) {
+	int i = o.startDate.compareTo(startDate);
+	if (i == 0)
+	    return id.compareTo(o.id);
+	return i;
+    }
+
+    @Override
+    public String toString() {
+	return name + " (" + Month.from(startDate) + " " + Year.from(startDate) + ")";
+    }
 }
